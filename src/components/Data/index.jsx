de9ec229c4b19d44a6411232ae122f0c89088d96
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-13 09:18:24 -0300

    loading logic

--- src/components/Data/index.jsx
+++ src/components/Data/index.jsx
@@ -3,19 +3,24 @@ import Context from '../../context'
 
 import Chart from './Chart'
 import DataTable from './DataTable'
+import Loading from '../Loading'
 
 import './style.css'
 
 export default function Data() {
-  const { data, userMsg } = useContext(Context)
+  const { data, userMsg, loading } = useContext(Context)
 
   return (
     <>
-      {!data.length ? <h2>{userMsg}</h2> : (
-        <div className='dataContainer'>
-          <DataTable />
-          <Chart />
-        </div>
+      {loading ? <Loading /> : (
+        <>
+          {!data.length ? <h2>{userMsg}</h2> : (
+            <div className='dataContainer'>
+              <DataTable />
+              <Chart />
+            </div>
+          )}
+        </>
       )}
     </>
   )
