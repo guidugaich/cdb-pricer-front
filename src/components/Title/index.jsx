ba0ac7ec22a087773e91e64724034855451a052a
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-13 00:32:17 -0300

    title component

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import './style.css'
+
 export default function index() {
   return (
-    <h1>
-      Precificador de CDB
-    </h1>
+    <div className='titleContainer'>
+      <h1>Precificador de CDB</h1>
+      <div className='infoContainer'>
+        <span>
+          Desenvolvido por <a href='https://github.com/guidugaich' target="_blank">guidugaich</a>
+        </span>
+        <span>
+          <a href='https://github.com/guidugaich/cdb-pricer' target="_blank">sobre o projeto</a>
+        </span>
+      </div>
+    </div>
   )
 }
