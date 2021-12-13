ab19a77b04dfe8866b2419e90af9e3dda3a76ccd
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-13 00:32:10 -0300

    input form component

--- src/components/InputForm/index.jsx
+++ src/components/InputForm/index.jsx
@@ -1,14 +1,44 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import Context from '../../context';
+
+import './style.css'
 
 export default function InputForm({ handleSubmit }) {
   const { setInvestmentDate, setCurrentDate, setCdbRate, setData } = useContext(Context)
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type='date' onChange={(e) => setInvestmentDate(e.target.value)}></input>
-      <input type='date' onChange={(e) => setCurrentDate(e.target.value)}></input>
-      <input type='number' onChange={(e) => setCdbRate(e.target.value)}></input>
+      <div className='inputContainer'>
+        <label htmlFor="investmentDate">Data de investimento:</label>
+        <input
+          id='investmentDate'
+          required
+          type='date'
+          onChange={(e) => setInvestmentDate(e.target.value)}
+        />
+      </div>
+      <div className="inputContainer">
+        <label htmlFor="currentDate">Data atual:</label>
+        <input
+          id="currentDate"
+          required
+          type='date'
+          onChange={(e) => setCurrentDate(e.target.value)}
+        />
+
+      </div>
+      
+      <div className="inputContainer">
+        <label htmlFor="cdbRate">Taxa do CDB:</label>
+        <input
+          id="cdbRate"
+          required
+          type='number'
+          step='0.01'
+          onChange={(e) => setCdbRate(e.target.value)}
+        />
+
+      </div>
 
       <button type='submit'>Calcular</button>
     </form>
